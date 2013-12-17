bf5117e8f2715a08936c9206b3d0a7dadaa167f4
Ardin Fardiansyah <fardiansyah.ardin@yahoo.com>
2013-12-17 13:26:42 +0700

    fix detail menambah delete menambah signout

--- src/clj/mobile_survey/core.clj
+++ src/clj/mobile_survey/core.clj
@@ -11,7 +11,7 @@
             [mobile-survey.common :refer [template-path base]]
             [mobile-survey.models.db :as models]
             [clj-time.format :as time-format]
-            [clj-time.core :refer [date-time]]
+            [clj-time.core :as time-core]
             [clj-time.coerce :refer [from-date]]))
 
 
@@ -19,7 +19,8 @@
     (cond
         (= view :create) '( {:tag :script, :attrs {:src "js/mobile-survey.js"}}
                             {:tag :script, :content ("mobile_survey.create_survey.init()")})
-        (= view :surveys) '({:tag :script, :attrs {:src "js/mobile-survey.js"}})))
+        (= view :surveys) '({:tag :script, :attrs {:src "js/mobile-survey.js"}})
+        (= view :detail)  '({})))
 
 (def status_code {0 "On progress", 1 "Complete"})
 
@@ -38,13 +39,21 @@
 (defsnippet create-snippet (str template-path "create-survey.html") [:#create-form]
     [])
 
+(defn get-duration [since]
+  (let [interval (time-core/interval since (time-core/now))]
+    (cond (not= (time-core/in-years interval) 0) (str (time-core/in-years interval) " year(s)")
+          (not= (time-core/in-days interval) 0) (str (time-core/in-days interval) " day(s)")
+          (not= (time-core/in-hours interval) 0) (str (time-core/in-hours interval) " hour(s)")
+          (not= (time-core/in-minutes interval) 0) (str (time-core/in-minutes interval) " minute(s)"))))
+
 (defsnippet detail-snippet (str template-path "detail.html") [:#survey-info]
-    [{:keys [name description created-on status]} number-count number-replied]
-    [:#name] (content name)
-    [:#description] (content description)
-    [:#duration] (content created-on)
-    [:#progress] (do-> (set-attr {:value number-replied :max number-count})
-                    (content (str number-replied " of " number-count " responded.")))
+    [{:keys [name description created_on status] :as survey} number-count number-replied]
+    [:td.name] (content name)
+    [:td.description] (content description)
+    [:td.content] (content (:content survey))
+    [:td.duration] (content (get-duration (from-date created_on)))
+    [:progress.progress] (set-attr :value number-replied :max number-count)
+    [:span.progress-num] (content (str number-replied " of " number-count " responded."))
     )
 
 (defn surveys [user-id]
@@ -74,7 +83,11 @@
 
 (defn detail-survey [id]
     (let [survey (models/get-survey-with-id id)]
-        (base (detail-snippet survey 10 100))))
+        (base (detail-snippet survey 100 10) (create-js-script-for :detail))))
+
+(defn delete-survey [id]
+  (models/delete-survey! id)
+  (redirect "/surveys"))
 
 (defn root [handler]
     (fn [request]
@@ -82,10 +95,16 @@
             "mobile_survey/templates/create-survey.html"
             (handler request))))
 
+(defn signout []
+  (session/clear!)
+  (redirect "/signin"))
+
 (def-restricted-routes app-routes
     (GET "/" [] (redirect "/surveys"))
     (GET "/surveys" [] (surveys (session/get :user_id)))
     (GET "/create" [] (create-survey-form))
+    (GET "/signout" [] (signout))
     (POST "/create" [name descriptions content incentiveOption numbersText numbersFile]
         (create-survey (session/get :user_id) name descriptions content incentiveOption numbersText numbersFile))
-    (GET ["/detail/:id", :id #"[0-9]+"] [id]  (detail-survey (int id))))
+    (GET ["/detail/:id", :id #"[0-9]+"] [id]  (detail-survey (Integer/parseInt id)))
+    (GET ["/detail/:id", :id #"[0-9]+"] [id] (delete-survey (Integer/parseInt id))))
