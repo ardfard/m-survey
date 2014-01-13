6bf5e08fd1e8f2534029a958a5488c59497de480
Ardin Fardiansyah <fardiansyah.ardin@yahoo.com>
2014-01-13 10:17:04 +0700

    fitur parsing banyak opsi

--- src/clj/mobile_survey/core.clj
+++ src/clj/mobile_survey/core.clj
@@ -12,7 +12,8 @@
             [clj-time.format :as time-format]
             [clj-time.core :as time-core]
             [clj-time.coerce :refer [from-date]]
-            [dk.ative.docjure.spreadsheet :as xls]))
+            [dk.ative.docjure.spreadsheet :as xls]
+            [clojure.string :refer [join]]))
 
 
 (defn create-js-script-for [view]
@@ -44,15 +45,25 @@
     (t/base (t/create-snippet) (:content (first (t/create-script)))))
 
 (defn create-survey-helper [values]
-  (models/create-survey! values)
-  (models/get-survey-id (:user_id values) (:name values)))
+  (println values)
+  #_(models/create-survey! values)
+  #_(models/get-survey-id (:user_id values) (:name values)))
 
 (defn handle-file-number [file-number]
     (let [file-num (io/upload-file "/temp/" file-number)]
       (get-numbers-from-file (str "resources/public/temp/" (file-number :filename)))))
 
-(defn create-survey [user-id name descriptions content selectNumber incentiveOption numbersText numbersFile]
-  (let [survey-id (create-survey-helper {:name name
+(defn create-survey [{:keys [user-id name descriptions content selectNumber
+                             incentiveOption numbersText numbersFile] :as params}]
+  (let [options (map val (dissoc params :user-id :name :descriptions :content :selectNumber
+                               :incentiveOption :numbersText :numbersFile))
+        option-letters (map #(format "(%s) " %) "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        options (->> options
+                     (map list option-letters)
+                     (map #(apply str %))
+                     (join \newline))
+        content (str content \newline options)
+        survey-id (create-survey-helper {:name name
                           :description descriptions
                           :content content
                           :incentive 0
@@ -62,11 +73,11 @@
                     (clojure.string/split (clojure.string/triml numbersText) #"\s+")
                     (handle-file-number numbersFile))]
       (doseq [number numbers]
-        (models/create-number! {:survey_id survey-id
+        #_(models/create-number! {:survey_id survey-id
                                :number number
                                :reply ""}))
-      (sms/publish-survey! {:id survey-id :content content} numbers)
-      (redirect "/surveys")))
+      #_(sms/publish-survey! {:id survey-id :content content} numbers)
+      #_(redirect "/surveys")))
 
 (defn detail-survey [id]
     (let [{:keys [name description created_on content status]} (models/get-survey-with-id id)
@@ -109,7 +120,7 @@
     (GET "/surveys" [] (surveys (session/get :user_id)))
     (GET "/create" [] (create-survey-form))
     (GET "/signout" [] (signout))
-    (POST "/create" {params :params} (do (println params) (str params)))
+    (POST "/create" {params :params} (create-survey params))
     (GET ["/detail/:id", :id #"[0-9]+"] [id]  (detail-survey (Integer/parseInt id)))
     (GET ["/delete/:id", :id #"[0-9]+"] [id] (delete-survey (Integer/parseInt id)))
     (GET ["/numbers/:id", :id #"[0-9]+"] [id] (view-number (Integer/parseInt id)))
